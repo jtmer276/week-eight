3891a2a3202d301b074b5623c61fd342d043d533
jtmer276 <jtmerrill276@gmail.com>
2024-03-25 14:59:02 -0400

    finished factorial began OddSum

---
+++ factorial.java
@@ -0,0 +1,23 @@
+/********
+Name: Jules Merrill
+Date: 3-25-3034
+Project: Factorial
+ ******/
+
+import java.util.*;
+
+public class factorial{
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = input.nextInt();
+        int num2 = 1;
+
+        for(int counter = num; counter != 0; counter-- ){
+            num2 *= num;
+            num --;
+        }
+        System.out.println(num2);
+        input.close();
+    }
+}
